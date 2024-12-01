3948cc6af69c1182b60cbfb7cc25f8a38263692b
Camila Novaes <novaes.csw@gmail.com>
2024-12-01 19:39:27 -0300

    update gemm.py

--- gemm.py
+++ gemm.py
@@ -1,10 +1,10 @@
 import os
 import time
 
-# os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
 import numpy as np
 
-N = 4098
+N = 4096
 
 if __name__ == "__main__":
     while True:
@@ -15,12 +15,15 @@ if __name__ == "__main__":
         C = A @ B
         e = time.monotonic()
 
-        flops = N * N * 2 * N  # rows * cols * sum items
+        flops = N * N * 2 * N  # rows * cols * dot product * 2 op (mul + add)
         t = e - s
-        print(f"{flops/t * 1e-9:.2f} GFLOP/s")
+        print(f"{flops/t * 1e-9:.2f} GFLOP/s ({t*1e3:.2f} ms)")
         time.sleep(0.5)
 
 
+# Notes:
+# - Numpy uses a highly optimized BLAS implementation.
+
 # Results:
-# 1.07 TFLOP/s using Ryzen 9 5900x (all threads)
-# 135.7 GFLOP/s using Ryzen 9 5900x (1 threads)
+# N = 4096 => 1.07 TFLOP/s using Ryzen 9 5900x (all threads)
+# N = 4096 => 135.7 GFLOP/s using Ryzen 9 5900x (1 threads)
